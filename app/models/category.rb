3e1442277b9ad41bfb6576a74037a64ddbd956de
devkook <devinkook@gmail.com>
2014-10-23 07:00:40 +0000

    post <-> category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category
   include Mongoid::Document
+  has_many :posts
   field :name, type: String
   field :slug, type: String
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post
   include Mongoid::Document
+  belong_to :category
   field :title, type: String
   field :body, type: String
 end
